18bb5ce3caca11f67226f1884931ee72b74d6da9
dcschreck <dcschreck421@gmail.com>
2017-11-14 19:47:11 -0500

    Checkpoint 8: completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,25 +1,23 @@
 (function() {
-    /**
-    * @function SongPlayer
-    * @desc Public service that plays or pauses audio file
-    * @returns {Object} SongPlayer
-    */
-    function SongPlayer() {
+    function SongPlayer(Fixtures) {
         /**
         * @desc Object that is returned by SongPlayer service
         * @type {Object}
         */
         var SongPlayer = {};
+
         /**
-        * @desc Song that is currently being played or has been paused
+        * @desc Gets and stores current album
         * @type {Object}
         */
-        var currentSong = null;
+        var currentAlbum = Fixtures.getAlbum();
+
         /**
         * @desc Buzz object audio file
         * @type {Object}
         */
         var currentBuzzObject = null;
+
         /**
         * @function setSong
         * @desc Stops currently playing song and loads new audio file as currentBuzzObject
@@ -28,7 +26,7 @@
         var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
-                currentSong.playing = null;
+                SongPlayer.currentSong.playing = null;
             }
 
             currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -36,8 +34,9 @@
                 preload: true
             });
 
-            currentSong = song;
+            SongPlayer.currentSong = song;
         };
+
         /**
         * @function playSong
         * @desc Sets currentBuzzObject to play and playing property of the song to true
@@ -47,36 +46,73 @@
             currentBuzzObject.play();
             song.playing = true;
         }
+
+        /**
+        * @function getSongIndex
+        * @desc Find index of currently playing song
+        * @param {Object} song
+        * @return {number}
+        */
+        var getSongIndex = function(song) {
+            return currentAlbum.songs.indexOf(song);
+        };
+
+        /**
+        * @desc Song that is currently being played or has been paused
+        * @type {Object}
+        */
+        SongPlayer.currentSong = null;
+
         /**
         * @function SongPlayer.play
         * @desc Public method that plays audio file
         * @param {Object} song
         */
         SongPlayer.play = function(song) {
-            if (currentSong !== song) {
+            song = song || SongPlayer.currentSong;
+            if (SongPlayer.currentSong !== song) {
                 setSong(song);
                 playSong(song);
-                // currentBuzzObject.play();
-                // song.playing = true;
-            } else if (currentSong === song) {
+            } else if (SongPlayer.currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
+                    playSong(song);
                 }
             }
         };
+
         /**
-        * @function SongPlayer.play
+        * @function SongPlayer.pause
         * @desc Public method that pauses audio file
         * @param {Object} song
         */
         SongPlayer.pause = function(song) {
+            song = song || SongPlayer.currentSong;
             currentBuzzObject.pause();
             song.playing = false;
         };
+
+        /**
+        * @function SongPlayer.previous
+        * @desc Method that goes to the previous song
+        */
+        SongPlayer.previous = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex--;
+
+            if (currentSongIndex < 0) {
+                currentBuzzObject.stop();
+                SongPlayer.currentSong.playing = null;
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
+        };
+
         return SongPlayer;
     }
 
     angular
         .module('blocJams')
-        .factory('SongPlayer', SongPlayer);
+        .factory('SongPlayer', ['Fixtures', SongPlayer]);
 })();
